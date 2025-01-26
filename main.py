8d051a59c283d8461d1618c5cf15eb326d119df4
dmitreiro <dario.mitreiro@ua.pt>
2025-01-26 23:17:33 +0000

    added main script file

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    pass
+    # csv compilation
+    # data filter
+    # data setup
+    # mesh interpolation
+    # reverse interpolation
+    # training
+    # testing
+    # plots
